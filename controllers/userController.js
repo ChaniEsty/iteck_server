86df1a48195782b6724030de5f9c3e6fd6055a09
ChaniEsty <36539137033@mby.co.il>
2023-05-16 00:09:00 +0300

    added else to send sv

--- controllers/userController.js
+++ controllers/userController.js
@@ -2,39 +2,39 @@ const userDal = require("../dal/userAccessor");
 const jobDal = require("../dal/jobsAccesor");
 const verifyJWT = require("../middleware/verifyJWT");
 const email = require("../email");
-const validation=require("validator");
+const validation = require("validator");
 
 class UserController {
-    getUsers=async(req,res)=>{
-        const users=await userDal.getUsers();
-        if(users)
+    getUsers = async (req, res) => {
+        const users = await userDal.getUsers();
+        if (users)
             res.status(200).json(users);
         else
             res.status(400).send("no users");
     }
-    getUserById=async(req,res)=>{
-        const id=req.params;
-        const user=await userDal.getUserById(id);
-        if(user)
+    getUserById = async (req, res) => {
+        const id = req.params;
+        const user = await userDal.getUserById(id);
+        if (user)
             res.status(200).json(user);
         res.status(400).send("no user found");
     }
     updateDetailes = async (req, res) => {
-        const id=req.params;
+        const id = req.params;
         const { email, iduser, name, phone, password } = req.body;
-        if (!email || !iduser || !password) 
+        if (!email || !iduser || !password)
             return res.status(400).send('All fields are required');
-        if(!validation.isEmail(email))
+        if (!validation.isEmail(email))
             return res.status(400).send('wrong email');
-        if(!validation.idId(iduser))
+        if (!validation.idId(iduser))
             return res.status(400).send('wrong id');
-        if(!validation.isPhone(phone))
+        if (!validation.isPhone(phone))
             return res.status(400).send('wrong phone number');
-        const updated = await userDal.updateDetailes(id,{ email, iduser, name, phone, password });  
-        if(updated)
+        const updated = await userDal.updateDetailes(id, { email, iduser, name, phone, password });
+        if (updated)
             res.status(200).send("user updated");
         else
-            res.status(400).send("could not update");  
+            res.status(400).send("could not update");
         // const deleted = await userDal.deleteUser({iduser});
         // if (deleted) {
         //     const created = await userDal.createUser({ email, iduser, name, phone, password });
@@ -47,7 +47,7 @@ class UserController {
     updateJobRequirments = async (req, res) => {
         const { field, subject, city } = req.query;
         const userId = req.user.email;
-        console.log(userId+"0000000000000000000000000");
+        console.log(userId + "0000000000000000000000000");
         const update = await userDal.updateJobRequirments(userId, field, subject, city);
         if (update.modifiedCount == 0)
             res.json(update);
@@ -55,7 +55,7 @@ class UserController {
             const jobList = await jobDal.getJobs(field, subject, city);
             userDal.deleteUserJobs(userId);
             jobList.forEach(async job => {
-            await userDal.addJobToUser(job.id,userId);
+                await userDal.addJobToUser(job.id, userId);
             });
             // res.json(jobList);
         }
@@ -76,13 +76,15 @@ class UserController {
     //     }
     // }
     sendCv = async (req, res) => {
-        console.log("manged to srend cv");
+        console.log("manged to send cv");
         const employerEmail = req.query.empId;
-        console.log(employerEmail+"999999999999999999999999999999999");
-        if(!validation.isEmail(employerEmail))
+        console.log(employerEmail + "999999999999999999999999999999999");
+        if (!validation.isEmail(employerEmail))
             res.send("wrong email");
-        email.sendEmail(employerEmail, "An employee just for you", req.body.toString());
-        res.send("cv sent");
+        else {
+            email.sendEmail(employerEmail, "An employee just for you", req.body.toString());
+            res.send("cv sent");
+        }
     }
 }
 const userController = new UserController();
